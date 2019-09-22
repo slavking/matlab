5ee364f37ca959ba5e6853225570c8655aec56f9
Stefan Colic <scolic@griddynamics.com>
2019-09-22 19:14:16 +0200

    dodat treci kvar

--- stefan_nesimetricni_rezim.m
+++ stefan_nesimetricni_rezim.m
@@ -42,6 +42,7 @@ varijante_sprega = [0,0 ;0,1;0,2;1,0;1,1;1,2;2,0;2,1;2,2]
 broj_redova = rows(varijante_sprega)
 brojac=broj_redova
 
+
 while (brojac > 0)
 	disp("naziv sprege = ")
 	disp(nazivi_sprega(brojac,1:4))
@@ -112,6 +113,7 @@ while (brojac > 0)
 	% NAPOMENA: Jednacine (1)-(3) zavise od vrste kvara; jednacina (6) zavisi
 	% od tipa sprege transformatora!!!
 	x0 = [1 1 1 1 1 1]; % vektor pocetnih vrednosti promenljivih
+	
 	x = fsolve(@(x) [(a^2*x(1)+a*x(2)+x(3))-(a*x(1)+a^2*x(2)+x(3)) - Rp*(a^2*x(4)+a*x(5)+x(6));
 	                 (a^2*x(4)+a*x(5)+x(6))+(a*x(4)+a^2*x(5)+x(6));
 	                 x(4)+x(5)+x(6);
@@ -119,6 +121,7 @@ while (brojac > 0)
 	                 Ui1 - Zk*x(5) - x(2);
 	                 U01 - Zk*x(6) - x(3)], x0);
 	
+	disp ("Spoj dve faze preko impedanse Zk")
 	% Velicine u di0 sistemu:
 	Ud2 = x(1)
 	Ui2 = x(2)
@@ -127,15 +130,89 @@ while (brojac > 0)
 	Ii2 = x(5)
 	I02 = x(6)
 	
-	% Fazni naponi i struje sekundara:
-	Ua2 = Ud2+Ui2+U02
-	Ub2 = a^2*Ud2+a*Ui2+U02
-	Uc2 = a*Ud2+a^2*Ui2+U02
+	 disp("Fazni naponi i struje sekundara:")
+     Ua2 = Ud2+Ui2+U02
+     Ub2 = a^2*Ud2+a*Ui2+U02
+	 Uc2 = a*Ud2+a^2*Ui2+U02
 	
-	Ia2 = Id2+Ii2+I02
-	Ib2 = a^2*Id2+a*Ii2+I02
-	Ic2 = a*Id2+a^2*Ii2+I02
-	brojac=brojac-1
-endwhile
+	 Ia2 = Id2+Ii2+I02
+	 Ib2 = a^2*Id2+a*Ii2+I02
+	 Ic2 = a*Id2+a^2*Ii2+I02
 
+
+    %jednofazni kratak spoj K1Z(a)
+    %Sta je Zf? Ja sam stavio da bude jednak Zk (impedansi kratkog spoja)
+    disp("Jednofazni kratak spoj - kvar za trougao i za zvezdu")
+    Zf=Rp %aktivni potrosac 
+    %Sve jednacine se pisu u obliku f(x)=0 i moraju se dovesti u taj oblik
+    %da bi funkcija fsolve prihvatila da radi
+    % (1) Ua2-Zf*Ia2=0
+    % (2) Ib2=0
+    % (3) Ic2=0
+    % (4) Ud2+Ui2+U02-Zf*(Id2+Ii2+I02)=0
+    % (5) a^2*Id2+a*Ii2+I02=0
+    % (6) a*Id2+a^2*Ii2+I02=0
+    % *** 6 jednacina, 6 nepoznatih (Ud2, Ui2, U02,Id2,Ii2,I02) ***
+    %%%%%%    %%%%%%    %%%%%%    %%%%%%    %%%%%%    %%%%%%    %%%%%%
+    % Resavamo jednacine sistema pomocu fsolve, koristimo postojece promenljive
+    % jednacina koja zavisi od tipa sprege transformatora je vec pokrivena programom
+    % z(1) = Ud2
+    % z(2) = Ui2
+    % z(3) = U02
+    % z(4) = Id2
+    % z(5) = Ii2
+    % z(6) = I02
+
+    z0 = [1 1 1 1 1 1];
+    z = fsolve(@(z) [ Ua2-Zf*Ia2; 
+                    Ib2; 
+                    Ic2; 
+                    (z(1)+z(2)+z(3))-Zf*(z(4)+z(5)+z(6)); 
+                    a^2*z(4)+a*z(5)+z(6); 
+                    a*z(4)+a^2*z(5)+z(6)], z0);
+
+    Ud2 = z(1)
+    Ui2 = z(2)
+    U02 = z(3)
+    Id2 = z(4)
+    Ii2 = z(5)
+    I02 = z(6)
+    
+    disp("trofazno nesimetricno opterecenje")
+    Za=Rp
+    Zb=Rp
+    Zc=Rp
+    % (1)   Ua2-Za*Ia2=0
+    % (2)   Ub2-Zb*Ib2=0
+    % (3)   Uc2-Zc*Ic2=0
+    % (4)   Ud2+Ui2+U02-Za*(Id2+Ii2+I02) = 0
+    % (5)   a^2*Ud2+a*Ui2+U02-Zb*(a^2*Id2+a*Ii2+I02) = 0
+    % (6)   a*Ud2+a^2*Ui2+U02-Zc*(a*Id2+a^2*Ii2+I02) = 0
+    % y(1) = Ud2
+    % y(2) = Ui2
+    % y(3) = U02
+    % y(4) = Id2
+    % y(5) = Ii2
+    % y(6) = I02
+
+
+    y0 = [1 1 1 1 1 1];
+    y = fsolve(@(y) [ Ua2-Za*Ia2; 
+                    Ub2-Zb*Ib2; 
+                    Uc2-Zc*Ic2; 
+                    y(1)+y(2)+y(3))-Za*(y(4)+y(5)+y(6)); 
+                    a^2*y(1)+a*y(2)+y(3)-Zb*(a^2*y(4)+a*y(5)+y(6)); 
+                    a*y(1)+a^2*y(2)+y(3)-Zc*(a*y(4)+a^2*y(5)+y(6))], y0);
+                    
+    Ud2 = y(1)
+    Ui2 = y(2)
+    U02 = y(3)
+    Id2 = y(4)
+    Ii2 = y(5)
+    I02 = y(6)
+    
+    
+    
+    brojac=brojac-1
+endwhile
 
